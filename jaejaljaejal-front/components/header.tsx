010f969dc91fe22581303b1d66ad6e77b0ac7a12
박준형 <108733795+krapli441@users.noreply.github.com>
2024-05-07 16:47:54 +0900

    header 추가

---
+++ jaejaljaejal-front/components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="w-full h-20vh bg-white flex justify-center items-center">
+      <h1>My Header</h1>
+    </header>
+  );
+};
+
+export default Header;
