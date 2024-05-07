001f8f54ffd238f9e4df56e822f132f81980354e
박준형 <108733795+krapli441@users.noreply.github.com>
2024-05-07 16:58:27 +0900

    header 텍스트 추가

--- jaejaljaejal-front/components/header.tsx
+++ jaejaljaejal-front/components/header.tsx
@@ -1,7 +1,15 @@
 const Header = () => {
   return (
-    <header className="w-full h-20vh bg-white flex justify-center items-center">
-      <h1>My Header</h1>
+    <header className="w-full h-20vh bg-white flex justify-between items-center px-10">
+      <h1 className="text-black">재잘재잘</h1>
+      <div>
+        <button className="text-white bg-blue-500 hover:bg-blue-700 font-medium py-2 px-4 rounded">
+          로그인
+        </button>
+        <button className="ml-4 text-white bg-green-500 hover:bg-green-700 font-medium py-2 px-4 rounded">
+          회원가입
+        </button>
+      </div>
     </header>
   );
 };
