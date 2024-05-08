0eaba3fcbfb5b4dbd828912bb782c4b591a3b6e9
박준형 <108733795+krapli441@users.noreply.github.com>
2024-05-08 13:22:37 +0900

    로그인 / 로그아웃 상태 추가

--- jaejaljaejal-front/components/header.tsx
+++ jaejaljaejal-front/components/header.tsx
@@ -1,11 +1,44 @@
+"use client";
+
+import { useState } from "react";
+
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 로그인
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  // 로그아웃
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <header className="w-full h-20vh bg-white flex justify-between items-center px-10">
       <h1 className="text-black text-2xl font-bold">재잘재잘</h1>
       <div>
-        <button className="text-black bg-white border border-gray-400 hover:bg-gray-100 font-medium py-2 px-4 rounded-lg">
-          회원가입 / 로그인
-        </button>
+        {isLoggedIn ? (
+          // 로그인
+          <>
+            <span className="text-black mr-4">사용자 정보</span>
+            <button
+              onClick={handleLogout}
+              className="text-white bg-red-500 hover:bg-red-700 font-medium py-2 px-4 rounded-lg"
+            >
+              로그아웃
+            </button>
+          </>
+        ) : (
+          // 비로그인
+          <button
+            onClick={handleLogin}
+            className="text-black bg-white border border-gray-400 hover:bg-gray-100 font-medium py-2 px-4 rounded-lg"
+          >
+            회원가입 / 로그인
+          </button>
+        )}
       </div>
     </header>
   );
