3fa399bcfdf3ee517bf63db82ddde491957f8b44
박준형 <108733795+krapli441@users.noreply.github.com>
2024-05-08 15:45:29 +0900

    텍스트 길이 수정

--- jaejaljaejal-front/app/page.tsx
+++ jaejaljaejal-front/app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <main className="bg-custom flex min-h-screen flex-col items-center">
       <Header />
-      <h1 className="w-full h-90vh text-3xl font-bold flex items-center justify-center text-9xl">
-        재잘재잘
-      </h1>
+      <div className=" h-90vh flex flex-col justify-center items-center">
+        <h1 className="font-bold text-9xl">재잘재잘</h1>
+        <h1 className="font-thin text-2xl">모임을 재미있고 편하게</h1>
+      </div>
     </main>
   );
 }
