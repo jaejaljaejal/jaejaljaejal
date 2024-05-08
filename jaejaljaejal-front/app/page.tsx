af099e20ee589a55e4bd0af058eb538a30f0367f
박준형 <108733795+krapli441@users.noreply.github.com>
2024-05-08 16:06:33 +0900

    부제 수정

--- jaejaljaejal-front/app/page.tsx
+++ jaejaljaejal-front/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="bg-custom flex min-h-screen flex-col items-center">
       <Header />
       <div className=" h-90vh flex flex-col justify-center items-center">
-        <h1 className="font-bold text-9xl">재잘재잘</h1>
-        <h1 className="font-thin text-2xl">모임을 재미있고 편하게</h1>
+        <p className="w-96 font-bold text-8xl">재잘재잘</p>
+        <p className="w-96 font-thin text-2xl">모임을 재미있고 편하게</p>
       </div>
     </main>
   );
