4a8dacb1facc018998d8d7f15b735ecb6e9a9c74
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-26 15:39:34 +0900

    placeholder 추가

--- jaejaljaejal-front/app/signup/page.tsx
+++ jaejaljaejal-front/app/signup/page.tsx
@@ -9,27 +9,39 @@ const SignupPage = () => {
       <Header />
       <div className="w-screen bg-white h-90vh flex flex-col items-center justify-center">
         <p className="w-80 mb-6 text-black text-2xl font-bold">회원가입</p>
-        <form className="flex flex-col space-y-4">
-          <input
-            type="text"
-            placeholder="아이디"
-            className="w-80 h-12 border border-black border-1 p-2 rounded-lg text-black"
-          />
-          <input
-            type="email"
-            placeholder="이메일"
-            className="w-80 h-12 border border-black border-1 p-2 rounded-lg text-black"
-          />
-          <input
-            type="password"
-            placeholder="비밀번호"
-            className="w-80 h-12 border border-black border-1 p-2 rounded-lg text-black"
-          />
-          <input
-            type="password"
-            placeholder="비밀번호 확인"
-            className="w-80 h-12 border border-black border-1 p-2 rounded-lg text-black"
-          />
+        <form className="flex flex-col space-y-6">
+          <div className="flex flex-col w-80">
+            <p className="text-black text-lg">아이디</p>
+            <input
+              type="text"
+              placeholder="4~20자리 / 영문, 숫자, 특수문자 '_' 사용 가능"
+              className="w-full h-12 border border-black border-1 p-2 rounded-lg text-black"
+            />
+          </div>
+          <div className="flex flex-col w-80">
+            <p className="text-black text-lg">이메일</p>
+            <input
+              type="email"
+              placeholder="유효한 이메일 주소"
+              className="w-full h-12 border border-black border-1 p-2 rounded-lg text-black"
+            />
+          </div>
+          <div className="flex flex-col w-80">
+            <p className="text-black text-lg">비밀번호</p>
+            <input
+              type="password"
+              placeholder="8~16자리 / 영문 대소문자, 숫자, 특수문자 조합"
+              className="w-full h-12 border border-black border-1 p-2 rounded-lg text-black"
+            />
+          </div>
+          <div className="flex flex-col w-80">
+            <p className="text-black text-lg">비밀번호 확인</p>
+            <input
+              type="password"
+              placeholder="비밀번호 재입력"
+              className="w-full h-12 border border-black border-1 p-2 rounded-lg text-black"
+            />
+          </div>
           <button
             type="submit"
             className="w-80 h-12 px-4 py-2 bg-custom text-white rounded-lg hover:bg-custom"
