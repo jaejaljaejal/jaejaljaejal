81e4ac6b5b1adbba343b3999b26545a3549f49aa
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-30 14:55:00 +0900

    Screen 추가

--- jaejaljaejal-front/app/signup/page.tsx
+++ jaejaljaejal-front/app/signup/page.tsx
@@ -100,12 +100,7 @@ const SignupPage = () => {
     <main className="bg-custom flex min-h-screen flex-col items-center">
       <Header />
       <div
-        className="w-screen bg-white flex flex-col items-center justify-center overflow-y-auto"
-        style={{
-          paddingTop: "12vh",
-          paddingBottom: "2vh",
-          height: "calc(100vh - 12vh)",
-        }}
+        className="w-screen h-screen bg-white flex flex-col items-center justify-center overflow-y-auto"
       >
         <p className="w-96 mb-6 text-black text-2xl font-bold">회원가입</p>
         <form className="flex flex-col space-y-6" onSubmit={handleSubmit}>
