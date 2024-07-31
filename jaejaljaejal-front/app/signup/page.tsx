c5a869949422f6b8cc8f1a2e6f8874aeabb28393
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-31 18:50:02 +0900

    스크롤 문제 수정

--- jaejaljaejal-front/app/signup/page.tsx
+++ jaejaljaejal-front/app/signup/page.tsx
@@ -5,7 +5,7 @@ const SignupPage = () => {
   return (
     <main className="bg-custom flex min-h-screen flex-col items-center">
       <Header />
-      <div className="w-screen h-screen bg-white flex flex-col items-center justify-center overflow-y-auto">
+      <div className="w-screen bg-white flex flex-col items-center justify-center overflow-y-auto pt-16 pb-16">
         <p className="w-96 mb-6 text-black text-2xl font-bold">회원가입</p>
         <SignupForm />
       </div>
