3bced676b4d7da92cee4c14c384af0749472ea2c
박준형 <108733795+krapli441@users.noreply.github.com>
2024-07-31 18:21:12 +0900

    성별 스타일링화

--- jaejaljaejal-front/app/signup/formRadioGroup.tsx
+++ jaejaljaejal-front/app/signup/formRadioGroup.tsx
@@ -22,22 +22,33 @@ const FormRadioGroup: React.FC<RadioGroupProps> = ({
   return (
     <div className="flex flex-col w-96 space-y-2">
       <p className="text-black text-md font-semibold">{label}</p>
-      <div className="flex justify-between">
+      <div className="flex space-x-2">
         {options.map((option) => (
-          <label key={option.value} className="flex items-center">
-            <input
-              type="radio"
-              name={name}
-              value={option.value}
-              checked={value === option.value}
-              onChange={onChange}
-              className="mr-2"
-              required={required}
-            />
+          <button
+            key={option.value}
+            type="button"
+            className={`flex-1 py-2 border rounded-lg ${
+              value === option.value
+                ? "bg-custom text-white"
+                : "bg-white text-black border-gray-300"
+            }`}
+            onClick={() =>
+              onChange({
+                target: { name, value: option.value },
+              } as React.ChangeEvent<HTMLInputElement>)
+            }
+          >
             {option.label}
-          </label>
+          </button>
         ))}
       </div>
+      <input
+        type="hidden"
+        name={name}
+        value={value}
+        required={required}
+        onChange={onChange}
+      />
       {error && <span className="text-red-500 text-xs">{error}</span>}
     </div>
   );
